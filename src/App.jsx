3330ea83b4fe21c039f1cc949d500301c8dd9313
parkqjunseo <127065961+parkqjunseo@users.noreply.github.com>
2023-10-06 21:47:20 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello, World!</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello, World!fff</h1>;
 }
 
 export default App;
