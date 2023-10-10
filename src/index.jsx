fe2e48fa6a003412a79be12f4087e214579ce641
parkqjunseo <junsns7@naver.com>
2023-10-10 16:41:33 +0900

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createStore } from 'redux';
 import ReactDOM from 'react-dom/client';
-
-import './index.css';
 import { Provider } from 'react-redux';
 import reducer from './common/reducer';
 import App from './App';
